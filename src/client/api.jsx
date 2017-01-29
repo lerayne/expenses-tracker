af4bf9de74e8e1219563ca346964c647a4f7ee77
Michael Yegorov <lerayne@gmail.com>
2017-01-30 00:47:29 +0200

    Testing new idea

--- src/client/api.jsx
+++ src/client/api.jsx
@@ -4,6 +4,43 @@
 
 import {httpDelete, httpGet, httpPost, httpPut} from './fetchHelpers'
 
+/*
+ * Идея - сделать так, чтобы объект API ниже и функция setApiListeners работали автоматически, на
+ * базе массива определений. Для этого, воможно, придется стандартизировать передаваемые в экшны
+ * параметры, но главная проблема в том, что придется вручную парсить URL-плейсхолдеры типа
+ * /api/transaction/:id
+ */
+
+// массив определений:
+/*const apiActions = [
+    {
+        method: 'get',
+        url: '/transactions',
+        name: 'getTransactions'
+    }, {
+        method: 'post',
+        url: '/transaction',
+        name: 'createTransaction'
+    }, {
+        method: 'delete',
+        url: '/transaction/:id',
+        name: 'deleteTransaction'
+    }
+]
+
+const newApi = {}
+
+apiActions.forEach(({name, url, method}) => {
+    newApi[name] = (params=[], body=false) => {
+
+        const placeholders = url.match(/:[a-z_$][a-z0-9_$]+/ig).map(exp => exp.replace(':', ''))
+
+        placeholders.forEach(paramName => url = url.replace(':'+paramName, params[paramName]))
+
+        return http[method](url, body)
+    }
+})*/
+
 const api = {
     getTransactions: () => httpGet('/api/transactions'),
     createTransaction: ta => httpPost('/api/transaction', ta),
