58eb1bbb4ae38ff13f7230b341c8349cd2613fd2
Michael Yegorov <lerayne@gmail.com>
2017-03-23 11:08:20 +0200

    rename non-jsx files to js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ require('babel-register')
 const notJS = ['.css', '.less', '.sass', '.ttf', '.woff', '.woff2']
 notJS.forEach(ext => require.extensions[ext] = () => {})
 require('babel-polyfill')
-require('./src/server.jsx')
+require('./src/server.js')
