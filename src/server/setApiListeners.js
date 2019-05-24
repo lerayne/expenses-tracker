5c049afdd8249feead023d735c75468cf637fa50
Michael Yegorov <lerayne@gmail.com>
2019-05-24 17:15:27 +0300

    simplified api listeners creation

--- src/server/setApiListeners.js
+++ src/server/setApiListeners.js
@@ -19,51 +19,45 @@ async function authorized(req, res) {
     }
 }
 
+async function ifAuthed(req, res, awaitable) {
+    if (await authorized(req, res)){
+        res.json(await awaitable())
+    }
+}
+
 export default function setApiListeners(app) {
 
     app.get('/api/transactions',
-        async(req, res) => {
-            if (await authorized(req, res)) {
-                const {dateFrom, dateTo} = req.query
-                res.json(await api.getTransactions(dateFrom, dateTo))
-            }
-        }
+        (req, res) => ifAuthed(req, res, () => {
+            const {dateFrom, dateTo} = req.query
+            return api.getTransactions(dateFrom, dateTo)
+        })
     )
 
     app.post('/api/transaction',
-        async(req, res) => await authorized(req, res) && res.json(await api.createTransaction(req.body))
+        (req, res) => ifAuthed(req, res, () => api.createTransaction(req.body))
     )
 
     app.put('/api/transaction/:id',
-        async(req, res) => await authorized(req, res) && res.json(await api.editTransaction(req.params.id, req.body))
+        (req, res) => ifAuthed(req, res, () => api.editTransaction(req.params.id, req.body))
     )
 
     app.delete('/api/transaction/:id',
-        async(req, res) => await authorized(req, res) && res.json(await api.deleteTransaction(req.params.id))
+        (req, res) => ifAuthed(req, res, () => api.deleteTransaction(req.params.id))
     )
 
     app.get('/api/summary',
-        async(req, res) => {
-            if (await authorized(req, res)) {
-                const {dateFrom, dateTo} = req.query
-                res.json(await api.getSummary(dateFrom, dateTo))
-            }
-        }
+        (req, res) => ifAuthed(req, res, () => {
+            const {dateFrom, dateTo} = req.query
+            return api.getSummary(dateFrom, dateTo)
+        })
     )
 
-    app.get('/api/categories',
-        async(req, res) => await authorized(req, res) && res.json(await api.getCategories())
-    )
+    app.get('/api/categories', (req, res) => ifAuthed(req, res, () => api.getCategories()))
 
-    app.post('/api/category',
-        async(req, res) => await authorized(req, res) && res.json(await api.createCategory(req.body))
-    )
+    app.post('/api/category', (req, res) => ifAuthed(req, res, () => api.createCategory(req.body)))
 
-    app.get('/api/stats',
-        async(req, res) => await authorized(req, res) && res.json(await api.getTotalStats())
-    )
+    app.get('/api/stats', (req, res) => ifAuthed(req, res, () => api.getTotalStats()))
 
-    app.get('/api/basic',
-        async (req, res) => await authorized(req, res) && res.json(await api.getBasicData())
-    )
+    app.get('/api/basic',(req, res) => ifAuthed(req, res, () => api.getBasicData()))
 }
