eaf873d3055bd70a8b022a6b7f30ddb062fc0d7c
Michael Yegorov <lerayne@gmail.com>
2019-09-15 16:19:39 +0300

    improved mobile view of TransactionsSummary

--- src/shared/components/TransactionsSummary/TransactionsSummary.jsx
+++ src/shared/components/TransactionsSummary/TransactionsSummary.jsx
@@ -4,6 +4,9 @@
 
 import React from 'react';
 import Table from 'react-bootstrap/lib/Table'
+import cn from 'classnames'
+
+import css from './TransactionsSummary.css'
 
 export default function TransactionsSummary(props) {
 
@@ -13,22 +16,45 @@ export default function TransactionsSummary(props) {
         expectedRemains,
     } = props
 
-    return <div className="TransactionsSummary">
-        <Table responsive condensed striped>
-            <thead>
-            <tr>
-                <th>Всего доходов</th>
-                <th>Всего расходов</th>
-                <th>Рассчетный остаток</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td className="text-success">{totalIncome}</td>
-                <td className="text-danger">{totalExpenses}</td>
-                <td>{expectedRemains}</td>
-            </tr>
-            </tbody>
-        </Table>
+    return <div className={cn(css.main, "TransactionsSummary")}>
+        <div className="desktop">
+            <Table responsive condensed striped>
+                <thead>
+                <tr>
+                    <th>Всего доходов</th>
+                    <th>Всего расходов</th>
+                    <th>Рассчетный остаток</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td className="text-success">{totalIncome}</td>
+                    <td className="text-danger">{totalExpenses}</td>
+                    <td>{expectedRemains}</td>
+                </tr>
+                </tbody>
+            </Table>
+        </div>
+
+        <div className="mobile">
+            <Table responsive condensed striped>
+                <tbody>
+                    <tr>
+                        <th>Всего доходов</th>
+                        <td className="text-success">{totalIncome}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Всего расходов</th>
+                        <td className="text-danger">{totalExpenses}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Рассчетный остаток</th>
+                        <td>{expectedRemains}</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
     </div>
 }
